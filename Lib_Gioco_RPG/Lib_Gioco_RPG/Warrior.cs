38b5e00ce8a7e056d99bfc3edcaa3a83f8f35715
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-02-23 13:01:06 +0100

    Add files via upload

---
+++ Lib_Gioco_RPG/Lib_Gioco_RPG/Warrior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Gioco_RPG
+{
+    public class Warrior:Character
+    {
+        int _forza;
+        public int Forza { get { return _forza; } set { _forza = value > 100 ? 100 : (value < 0 ? 0 : value); } }
+        public Warrior(string n) : base(n)
+        {
+            Forza = rnd.Next(1,101);
+        }
+        public override string Stato()
+        {
+            return Nome + $" ha {Vita} HP e {Forza} fP";
+        }
+    }
+}
