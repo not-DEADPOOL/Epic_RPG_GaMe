cd2d841fb10e9e743538778ea8a4c9417ff7b684
DEAD POOL <78198419+not-DEADPOOL@users.noreply.github.com>
2023-02-23 13:04:35 +0100

    Add files via upload

---
+++ RPG GAME/Lib_Gioco_RPG/Boss.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_Gioco_RPG
+{
+    class Boss
+    {
+        public string Nome;
+        int _vita;
+        public int Vita { get { return _vita; } set { _vita = value > 100 ? 100 : (value < 0 ? 0 : value); } }
+        int _attacco;
+        public int Attacco { get { return _attacco; } set { _attacco = value > 300 ? 300 : (value < 1 ? 1 : value); } }
+
+        public Boss(string nome, int vita, int attacco)
+        {
+            Nome = nome;
+            Vita = vita;
+            Attacco = attacco;
+        }
+        public string Stato()
+        {
+            return "Il BOSS " + Nome + " ha " + $"{Vita}" + " HP";
+        }
+    }
+}
---
+++ RPG GAME/Lib_Gioco_RPG/Character.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace Lib_Gioco_RPG
+{
+    public class Character
+    {
+        public string Nome;
+        int _vita;
+        public int Vita { get { return _vita; } set { _vita = value > 100 ? 100 : (value < 0 ? 0 : value); } }
+        protected Random rnd;
+
+        public Character(string nome)
+        {
+            Nome = nome;
+            rnd = new Random();
+            Vita = rnd.Next(1, 101);
+            Console.WriteLine("he, he, he ,wha");
+        }
+        public virtual string Stato()
+        {
+            return Nome + " ha " + $"{Vita}" + " HP";
+        }
+    }
+}
